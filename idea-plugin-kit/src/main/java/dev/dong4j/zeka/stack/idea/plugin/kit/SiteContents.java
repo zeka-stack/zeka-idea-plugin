677d372e5886ad0b2bce314c368015ac77697e2a
dong4j <dong4j@gmail.com>
2026-01-19 21:55:17 +0800

    feat(core): 新增 SiteContents 用于集中管理 url

---
+++ idea-plugin-kit/src/main/java/dev/dong4j/zeka/stack/idea/plugin/kit/SiteContents.java
@@ -0,0 +1,62 @@
+package dev.dong4j.zeka.stack.idea.plugin.kit;
+
+/**
+ *
+ *
+ * @author dong4j
+ * @version 1.0.0
+ * @email "mailto:dong4j@gmail.com"
+ * @date 2026.01.19 20:06
+ * @since x.x.x
+ */
+public class SiteContents {
+
+    /** 反馈接口地址 <a href="https://zekastack.dong4j.site/api/plugin/feedback">https://zekastack.dong4j.site/api/plugin/feedback</a> */
+    public static final String FEEDBACK_API_URL = "https://zekastack.dong4j.site/api/plugin/feedback";
+
+    /** 模型服务基础 API 地址 <a href="https://zekastack.dong4j.site/api/plugin/model">https://zekastack.dong4j.site/api/plugin/model</a> */
+    public static final String MODEL_API_BASE_URL = "https://zekastack.dong4j.site/api/plugin/model";
+
+    /** 上传文件的 API 地址 <a href="https://zekastack.dong4j.site/api/plugin/events">https://zekastack.dong4j.site/api/plugin/events</a> */
+    public static final String UPLOAD_URL = "https://zekastack.dong4j.site/api/plugin/events";
+
+    /** 隐私政策页面地址 <a href="https://zekastack.dong4j.site/#/privacy">https://zekastack.dong4j.site/#/privacy</a> */
+    public static final String PRIVACY = "https://zekastack.dong4j.site/#/privacy";
+    /** 数据管理页面地址 <a href="https://zekastack.dong4j.site/#/datas">https://zekastack.dong4j.site/#/datas</a> */
+    public static final String DATAS = "https://zekastack.dong4j.site/#/datas";
+    public static final String DONATE = "https://zekastack.dong4j.site/#/donate";
+
+    /** IDE 插件版本信息地址, 用于检查更新和获取版本详情 <a href="https://ideaplugin.dong4j.site/version">https://ideaplugin.dong4j.site/version</a> */
+    public static final String VERSION_URL = "https://ideaplugin.dong4j.site/version";
+
+    /**
+     * 开发者引擎入口地址, 用于访问引擎落地页
+     * <a href="https://ideaplugin.dong4j.site/engine/landing.html">https://ideaplugin.dong4j.site/engine/landing.html</a>
+     */
+    public static final String ENGINE = "https://ideaplugin.dong4j.site/engine/landing.html";
+    public static final String JAVADOC = "https://ideaplugin.dong4j.site/javadoc/landing.html";
+    public static final String CHANGELOG = "https://ideaplugin.dong4j.site/changelog/landing.html";
+    public static final String TRACER = "https://ideaplugin.dong4j.site/tracer/landing.html";
+    public static final String SWAGGER = "https://ideaplugin.dong4j.site/swagger/landing.html";
+
+
+    public static final String WHATSNEW = "https://ideaplugin.dong4j.site/whatsnew";
+
+
+    /** GitHub 仓库链接 */
+    public static final String GITHUB_LINK = "https://github.com/zeka-stack/zeka-idea-plugin";
+    /** GitHub Issues 链接 */
+    public static final String GITHUB_ISSUE_LINK = "https://github.com/zeka-stack/zeka-idea-plugin/issues/new";
+    /** 支持页面链接 */
+    public static final String SUPPORT_LINK = "https://plugins.jetbrains.com/plugin/29152";
+
+    /**
+     * 捐赠者列表链接
+     * <p> 指向包含捐赠者名单的博客页面.
+     */
+    public static final String DONORS_LIST_LINK = "https://blog.dong4j.site/about";
+    /** 插件市场链接, 存储插件在 JetBrains 插件市场的 URL */
+    public static final String MARKETPLACE_LINK = "https://plugins.jetbrains.com/plugin/29152";
+    /** 插件市场评价链接 */
+    public static final String MARKETPLACE_REVIEWS_LINK = "https://plugins.jetbrains.com/vendor/9afaba35-91ea-4364-8ced-64db868dd23e";
+}
