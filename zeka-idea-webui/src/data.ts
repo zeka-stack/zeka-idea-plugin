7e00c5d52b573eda828e627dab91caf9f3748a9d
dong4j <dong4j@gmail.com>
2026-01-23 17:23:33 +0800

    chore(core): 清理 zeka-idea-webui 项目全部文件

--- zeka-idea-webui/src/data.ts
+++
@@ -1,9 +0,0 @@
-import type {Feedback, FeedbackComment} from './lib/api';
-
-export type Comment = FeedbackComment;
-
-export interface Request extends Feedback {
-    commentsList?: Comment[];
-}
-
-export const requests: Request[] = [];
