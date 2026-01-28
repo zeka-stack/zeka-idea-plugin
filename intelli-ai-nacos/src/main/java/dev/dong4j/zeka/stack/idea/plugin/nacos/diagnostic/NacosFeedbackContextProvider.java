3bf4ae8178418777dd5a263c578b2644d65b67bf
dong4j <dong4j@gmail.com>
2026-01-28 23:33:14 +0800

    feat: 添加反馈上下文提供者类并注册到插件
    - 为 IntelliAI Javadoc 插件新增 FeedbackContextProvider 实现类, 用于标识上报来源
    - 在 plugin.xml 中注册该提供者, 确保反馈数据可被正确识别

---
+++ intelli-ai-nacos/src/main/java/dev/dong4j/zeka/stack/idea/plugin/nacos/diagnostic/NacosFeedbackContextProvider.java
@@ -0,0 +1,27 @@
+package dev.dong4j.zeka.stack.idea.plugin.nacos.diagnostic;
+
+import dev.dong4j.zeka.stack.idea.plugin.common.diagnostic.AbstractFeedbackContextProvider;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Nacos 反馈上下文提供者
+ * <p> 用于标识上报来源为 IntelliAI Nacos 插件.</p>
+ *
+ * @author dong4j
+ * @version 1.0.0
+ * @date 2026.01.28
+ * @since 1.0.0
+ */
+public class NacosFeedbackContextProvider extends AbstractFeedbackContextProvider {
+
+    /**
+     * 获取标题前缀
+     * <p> 重写父类方法, 返回 null, 表示不提供标题前缀 </p>
+     *
+     * @return 标题前缀, 此处返回 null
+     */
+    @Override
+    public @Nullable String getTitlePrefix() {
+        return null;
+    }
+}
---
+++ intelli-ai-swagger/src/main/java/dev/dong4j/zeka/stack/idea/plugin/swagger/diagnostic/SwaggerFeedbackContextProvider.java
@@ -0,0 +1,27 @@
+package dev.dong4j.zeka.stack.idea.plugin.swagger.diagnostic;
+
+import dev.dong4j.zeka.stack.idea.plugin.common.diagnostic.AbstractFeedbackContextProvider;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Swagger 反馈上下文提供者
+ * <p> 用于标识上报来源为 IntelliAI Swagger 插件.</p>
+ *
+ * @author dong4j
+ * @version 1.0.0
+ * @date 2026.01.28
+ * @since 1.0.0
+ */
+public class SwaggerFeedbackContextProvider extends AbstractFeedbackContextProvider {
+
+    /**
+     * 获取标题前缀
+     * <p> 重写父类方法, 返回 null, 表示不提供标题前缀 </p>
+     *
+     * @return null, 表示无标题前缀
+     */
+    @Override
+    public @Nullable String getTitlePrefix() {
+        return null;
+    }
+}
