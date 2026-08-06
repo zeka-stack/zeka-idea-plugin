5ac25cf985dd60054ac78c875b3d33bf04f7a2e3
dong4j <dong4j@gmail.com>
2026-08-06 23:26:06 +0800

    docs(core): 新增 2026_2_1002 版本更新说明文件

--- intelli-ai-engine/src/main/java/dev/dong4j/zeka/stack/idea/plugin/common/whatsnew/InternalWhatsNewProvider.java
+++ intelli-ai-engine/src/main/java/dev/dong4j/zeka/stack/idea/plugin/common/whatsnew/InternalWhatsNewProvider.java
@@ -24,6 +24,7 @@ public class InternalWhatsNewProvider implements WhatsNewProvider {
      */
     private static final List<WhatsNewPage> PAGES = List.of(
         // version mark
+        new DefaultWhatsNewPage("2026.2.1002", "2026_2_1002.html"),
         new DefaultWhatsNewPage("2026.2.1001", "2026_2_1001.html"),
         new DefaultWhatsNewPage("2026.1.2000", "2026_1_2000.html"),
         new DefaultWhatsNewPage("2026.1.1000", "2026_1_1000.html"),
